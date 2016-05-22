1722602fd17b7b1da1a346527779681ed443e279
tomlxq <21429503@qq.com>
2016-05-23 07:55:19 +0800

    add more codes

---
+++ asciidoc-study/src/main/java/com/example/Hello.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by tom on 2016/5/23.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello world!"); // # <1>
+    }
+}
